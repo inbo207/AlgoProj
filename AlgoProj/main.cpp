2cbf237bd2ea2762b1d8969bf43d412d3a480366
inbochoi <inbo207@gmail.com>
2020-07-23 00:27:01 +0900

    first commit

---
+++ AlgoProj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoProj
+//
+//  Created by inbochoi on 2020/07/23.
+//  Copyright © 2020 inbochoi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
